548890e861354c19d616050db1b98c3d99c095ae
Siraj Farhan <hello@getjamb.com>
2018-08-16 02:11:11 +0530

    completed plot 4

--- plot4.R
+++ plot4.R
@@ -9,12 +9,20 @@ dataset_path <- "/Users/siraj/projects/learning/datasciencecoursera/household_po
 power_data <- fread(dataset_path, na.strings=getOption("datatable.na.strings","?"))
 power_data <- power_data %>% mutate(DateTime = dmy_hms(paste(Date, Time))) %>% filter(DateTime >= "2007-02-01" & DateTime <= "2007-02-02 23:59:00")
 
-png("plot3.png", width = 480, height = 480)
+png("plot4.png", width = 480, height = 480)
+
+par(mfrow=c(2,2), mar=c(2,4,1,1))
+
+with(power_data, plot(DateTime, Global_active_power, type = "l",  ylab = "Global Active Power"))
+
+with(power_data, plot(DateTime, Voltage, type = "l",  ylab = "Voltage"))
 
 with(power_data, plot(DateTime, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "DateTime"))
 with(power_data, lines(DateTime, Sub_metering_2, type = "l", col = "red"))
 with(power_data, lines(DateTime, Sub_metering_3, type = "l", col = "blue"))
 
 legend("topright", legend=c("sub_metering_1","sub_metering_2","sub_metering_3"), lty = c(1, 1, 1), col = c("black","red","blue"))
+
+with(power_data, plot(DateTime, Global_reactive_power, type = "l",  ylab = "Global Reactive Power"))
 
 dev.off()
