3d98843d0656d4d39c81b51c118d0237f2a9e9f9
Siraj Farhan <hello@getjamb.com>
2018-08-16 01:25:22 +0530

    completed plot 2

--- plot2.R
+++ plot2.R
@@ -1,5 +1,16 @@
 library(data.table)
+library(dplyr)
+library(lubridate)
+
+Sys.setenv(TZ = "UTC")
 
 dataset_path <- "/Users/siraj/projects/learning/datasciencecoursera/household_power_consumption.txt"
 
 power_data <- fread(dataset_path, na.strings=getOption("datatable.na.strings","?"))
+power_data <- power_data %>% mutate(DateTime = dmy_hms(paste(Date, Time))) %>% filter(DateTime >= "2007-02-01" & DateTime <= "2007-02-02 23:59:00")
+
+png("plot2.png", width = 480, height = 480)
+
+with(power_data, plot(DateTime, Global_active_power, type = "l",  ylab = "Global Active Power (kilowatts)"))
+
+dev.off()
